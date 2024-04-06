d1e084173ded669a65d2ffda77b29b61c9a242e2
Ross Edlin <rossedlin@gmail.com>
2024-04-06 14:01:14 +0100

    Add NewRandomizerMethods

--- public/index.php
+++ public/index.php
@@ -2,7 +2,32 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use \App\EightTwo;
+use \App\EightThree;
+
 pre('Hi, this is PHP v8.3');
 
-pre(\App\EightTwo\TypedClassConstants::PHP);
-pre(\App\EightThree\TypedClassConstants::PHP);
+/**
+ * Typed Class Constants
+ */
+pre(EightTwo\TypedClassConstants::PHP); //Runs in v8.3 & v8.2
+//pre(EightThree\TypedClassConstants::PHP); //Fatal error in v8.3, runs in v8.2
+
+/**
+ *
+ */
+//$searchableConstant = 'PHP';
+
+//var_dump(constant(Foo::class . "::{$searchableConstant}"));
+
+/**
+ * New Randomizer::getBytesFromString() method
+ */
+pre(EightThree\NewRandomizerMethods::getBytesFromString());
+pre(EightThree\NewRandomizerMethods::getFloat());
+pre(EightThree\NewRandomizerMethods::nextFloat());
+
+/**
+ * Command line linter supports multiple files
+ */
+//php -l foo.php bar.php
